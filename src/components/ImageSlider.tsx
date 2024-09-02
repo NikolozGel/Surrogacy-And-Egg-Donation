7ef2ac6a059617f9ebe8c2f7678b1a149afa1c93
unknown <nikolozgelenidze9@gmail.com>
2024-09-03 02:39:34 +0400

    delete sliderImage full structure and create new

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,206 +1,42 @@
-import { SetStateAction, useState, useEffect } from "react";
-import "./ImageSlidesStyles.css";
+import { useState } from "react";
+import pregnantImage from "/images/parents-2.jpg";
+import parents from "/images/parents.webp";
 
-interface Slide {
-  url: string;
-}
+export default function ImageSlider() {
+  const images = [pregnantImage, parents]; // Array of images
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-interface Islides {
-  slides: Slide[];
-}
-
-const ImageSlider = ({ slides }: Islides) => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [isButtonActive, setIsButtonActive] = useState<boolean>(true);
-  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(
-    null
-  );
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(
-    null
-  );
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCurrentIndex((prevIndex) => {
-        const newIndex =
-          prevIndex === slides.length - 1 ? 0 : prevIndex + 1;
-        return newIndex;
-      });
-    }, 6000); // Change image every 6 seconds
-
-    setIntervalId(id);
-
-    // Clean up interval on component unmount
-    return () => clearInterval(id);
-  }, [slides.length]);
-
-  const resetInterval = () => {
-    if (intervalId) {
-      clearInterval(intervalId);
-    }
-
-    const id = setInterval(() => {
-      setCurrentIndex((prevIndex) => {
-        const newIndex =
-          prevIndex === slides.length - 1 ? 0 : prevIndex + 1;
-        return newIndex;
-      });
-    }, 6000);
-
-    setIntervalId(id);
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
-  const slideStyles = {
-    width: "100%",
-    height: "100%",
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    backgroundImage: `url(${slides[currentIndex].url})`,
-  };
-
-  const leftArrowStyles: React.CSSProperties = {
-    position: "absolute" as const,
-    top: "40%",
-    transform: "translate (0, -50%)",
-    left: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: "1",
-    cursor: "pointer",
-  };
-
-  const rightArrowStyles: React.CSSProperties = {
-    position: "absolute" as const,
-    top: "40%",
-    transform: "translate (0, -50%)",
-    right: "32px",
-    fontSize: "45px",
-    color: "#fff",
-    zIndex: "1",
-    cursor: "pointer",
-  };
-
-  const dotsContainerStyles = {
-    display: "flex",
-    justifyContent: "center",
-  };
-
-  const dotSTyles = {
-    margin: "0 3px",
-    cursor: "pointer",
-    fontSize: "20px",
-  };
-
-  const handleButtonClick = (callback: () => void) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-
-    setIsButtonActive(false);
-    callback();
-
-    const id = setTimeout(() => {
-      setIsButtonActive(true);
-    }, 1000); // 1 წამი
-
-    setTimeoutId(id);
-
-    // Reset automatic slide change interval
-    resetInterval();
-  };
-
-  const handleDotClick = (slideIndex: number) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-
-    setIsButtonActive(false);
-    goToSlide(slideIndex);
-
-    const id = setTimeout(() => {
-      setIsButtonActive(true);
-    }, 1000); // 1 წამი
-
-    setTimeoutId(id);
-
-    // Reset automatic slide change interval
-    resetInterval();
-  };
-
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide
-      ? slides.length - 1
-      : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (slideIndex: SetStateAction<number>) => {
-    setCurrentIndex(slideIndex);
+  const previousImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
 
   return (
-    <div className="h-[100%] relative flex flex-col items-center">
-      {/* ეს ორი არის ფოტოზე გადასასვლელი ღილაკი */}
-      <div className="hidden md:block">
-        <div
-          style={leftArrowStyles}
-          onClick={() =>
-            isButtonActive && handleButtonClick(goToPrevious)
-          }
-          className={`bg-orange-600 w-[70px] h-[70px] pl-6 rounded-full opacity-60 ${
-            isButtonActive
-              ? "hover:opacity-100 active:opacity-100"
-              : ""
-          }`}
-        >
-          {"<"}
-        </div>
-
-        <div
-          style={rightArrowStyles}
-          onClick={() =>
-            isButtonActive && handleButtonClick(goToNext)
-          }
-          className={`bg-orange-600 w-[70px] h-[70px] pl-6 rounded-full opacity-60 ${
-            isButtonActive
-              ? "hover:opacity-100 active:opacity-100"
-              : ""
-          }`}
-        >
-          {">"}
-        </div>
-      </div>
-      <button className="absolute bg-[#5dbea3] w-[240px] h-[60px] mt-[100px] rounded-3xl text-white hover:opacity-70 ease duration-300">
-        Contact Us To Start Now
+    <div className="flex justify-between relative ">
+      <button
+        className="bg-green-600 w-[70px] h-[70px] rounded-full absolute"
+        onClick={previousImage}
+      >
+        {"<"}
       </button>
-      <div style={slideStyles} className="slide"></div>
-      <div style={dotsContainerStyles}>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            style={dotSTyles}
-            onClick={() =>
-              isButtonActive && handleDotClick(slideIndex)
-            }
-            className={`text-orange-600 ${
-              isButtonActive
-                ? "hover:text-yellow-600 active:text-yellow-600"
-                : ""
-            }`}
-          >
-            {"●"}
-          </div>
-        ))}
+
+      <div className="w-[100%]">
+        <img src={images[currentIndex]} alt="Slideshow" />
       </div>
+
+      <button
+        className="bg-green-600 w-[70px] h-[70px] rounded-full absolute right-0"
+        onClick={nextImage}
+      >
+        {">"}
+      </button>
     </div>
   );
-};
-
-export default ImageSlider;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,12 @@
 import Header from "../components/Header";
 import ImageSlider from "../components/ImageSlider";
 
-type ISlides = {
-  url: string;
-  title: string;
-};
-
 export default function Home() {
-  // ესენი არის ფოტოებისთვის 'სლაიდერი'
-  const slides: ISlides[] = [
-    { url: "/images/happy-parents.jpg", title: "Parents1" },
-    { url: "/images/baby.jpg", title: "parents2" },
-    { url: "/images/happyBabe.jpg", title: "Parents3" },
-    { url: "/images/family-play.jpg", title: "Parents4" },
-    {
-      url: "/images/family-fun-time-stockcake.jpg",
-      title: "Parents5",
-    },
-  ];
-
   return (
-    <div className="relative">
+    <div>
       <Header />
 
-      <div className="w-[100%] h-[230px] sm:h-[330px]  md:h-[480px] xl:h-[730px]">
-        <ImageSlider slides={slides} />
-      </div>
+      <ImageSlider />
     </div>
   );
 }
