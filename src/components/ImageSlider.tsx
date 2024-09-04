a390f42b65e5105f9da66c9e64f251283c74218f
unknown <nikolozgelenidze9@gmail.com>
2024-09-04 13:05:42 +0400

    finish imageSliderStructure

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,45 +1,49 @@
 import { Carousel } from "antd";
-import { useRef, useEffect } from "react";
+import "./slider.css";
 
 const contentStyle: React.CSSProperties = {
   margin: 0,
-  height: "560px",
   color: "#fff",
   width: "100vw",
-  transition: "0.5s ease in-out",
 };
 
 const App: React.FC = () => {
   return (
-    <>
-      <Carousel arrows infinite={false}>
-        <div className="w-screen">
-          <h3>
-            <img src={"/images/smiling.jpg"} alt="" style={contentStyle} />
-          </h3>
-        </div>
-        <div className="w-screen">
-          <h3>
-            <img
-              src={"/images/family-fun-time-stockcake.jpg"}
-              alt=""
-              style={contentStyle}
-            />
-          </h3>
-        </div>
-        <div>
-          <h3>
-            <img src={"/images/family-play.jpg"} alt="" style={contentStyle} />
-          </h3>
-        </div>
+    <Carousel
+      className="custom-carousel"
+      arrows
+      infinite={true}
+      autoplay={true}
+      autoplaySpeed={5000}
+      speed={900}
+      effect="fade"
+    >
+      <div className="w-screen">
+        <h3>
+          <img src={"/images/smiling.jpg"} alt="" style={contentStyle} />
+        </h3>
+      </div>
+      <div className="w-screen">
+        <h3>
+          <img
+            src={"/images/family-fun-time-stockcake.jpg"}
+            alt=""
+            style={contentStyle}
+          />
+        </h3>
+      </div>
+      <div>
+        <h3>
+          <img src={"/images/family-play.jpg"} alt="" style={contentStyle} />
+        </h3>
+      </div>
 
-        <div>
-          <h3>
-            <img src={"/images/parents-2.jpg"} alt="" style={contentStyle} />
-          </h3>
-        </div>
-      </Carousel>
-    </>
+      <div>
+        <h3>
+          <img src={"/images/parents-2.jpg"} alt="" style={contentStyle} />
+        </h3>
+      </div>
+    </Carousel>
   );
 };
 
